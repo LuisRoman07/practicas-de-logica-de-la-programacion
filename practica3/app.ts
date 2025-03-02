e218464c88dfbb38137b88326025771b56259abb
LuisRoman07 <luisroman2908@gmail.com>
2025-03-02 17:10:52 -0600

    adding practice 2 y practice 3
    clear

---
+++ practica3/app.ts
@@ -0,0 +1,26 @@
+
+
+let names : string = "Roman"
+
+let age : number = 17
+
+let activo : boolean = true
+
+interface user {
+  name : string,
+   age : number,
+   email : string,
+}
+
+const sumando = (num1:number , num2:number):number => {
+
+  return num1 + num2
+
+}
+
+
+
+
+
+
+const listUser: user[] = []
