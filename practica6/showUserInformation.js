2089b143c7c8629f677e6cb42d609e36cd31ae77
LuisRoman07 <luisroman2908@gmail.com>
2025-03-30 12:18:39 -0600

    creamos una funcion que crea una computadora y retorna la informacion y dimos un breve repaso a las practicas anteriores

---
+++ practica6/showUserInformation.js
@@ -0,0 +1,28 @@
+let name = "Roman" //String --- cadena de texto
+
+let age = 21.5 //Number puede ser que sea un dato numerico o un dato float (DECIMAL)
+
+
+let number = 22.22 // float o double
+
+
+// en linea o no esta en linea
+
+let isActive = true // true o false
+
+isActive = false
+
+if(isActive == true){
+    return 'en linea'
+}else {
+    return 'ulima vez 02 PM'
+}
+
+const siEstaActivo = {} =>
+
+const fruits = [
+    "apple" ,
+    "pear" ,
+    "uva" ,
+    1
+]
